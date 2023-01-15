5ab45bf2b575df3d23699ad8e2d9d5c061faf8e2
Temi Sam <87991183+Temitopesam1@users.noreply.github.com>
2023-01-15 11:48:30 +0100

    Update 4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Script of a flask framework project """                                                                                                                                                                                                                                                                                                                                                     
+""" Script of a flask framework project """
 from flask import Flask, render_template
 
 
